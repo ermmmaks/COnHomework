03eaa15da2cd7bad4c530da7e31c851f3945ecc3
ermmmaks <maks.ermoshkin56@gmail.com>
2026-03-31 01:06:00 +0300

    Remove reducent strings in .h

--- CSVPrettyPrinter/CSVPretPrin.h
+++ CSVPrettyPrinter/CSVPretPrin.h
@@ -1,13 +1,5 @@
 #pragma once
 
-#ifndef CSV_PRET_PRIN_H
-#define CSV_PRET_PRIN_H
-
 #include <stdio.h>
 
-#define MAX_CELL_LEN 256
-#define MAX_COLS 50
-
 void algCSV(const char* inPath, const char* outPath);
-
-#endif
