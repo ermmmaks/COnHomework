9d1d7881ab6167909e9e98c23611af1c5fab773d
ermmmaks <maks.ermoshkin56@gmail.com>
2026-04-07 23:49:04 +0300

    Fix build

--- H4.States/states.h
+++ H4.States/states.h
@@ -12,5 +12,5 @@ Graph* graphCreate(int n, int m);
 void graphAdd(Graph* g, int u, int v, int len, int idx);
 void graphFree(Graph* g);
 
-int findNearest(Graph* g, State* s, int* sityesOwners);
+int findNearest(Graph* g, State* s, const int* sityesOwners);
 void annexation(Graph* g, State* s, int k, int* sityesOwners);
